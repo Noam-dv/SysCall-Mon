b893cd4769e68c17984f679b7e2fefb78246b84a
Noam <149065490+Noam-dv@users.noreply.github.com>
2026-01-27 10:36:21 +0200

    changed documentation

--- syscall_tracer.c
+++ syscall_tracer.c
@@ -1,31 +1,26 @@
 //ebpf program
 //runs in kernel
 
-//HEAVILY commented just to help me remember too cuz tihs looks way more complex than it is
-
-
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h> 
 
-struct syscall_evt { //event we send to user space
-    u32 pid; //kinda mimic our syscall dataclass
+struct syscall_evt { //event we send to user space (mimic our SysCall dataclass)
+    u32 pid;  
     u64 id;
-    u64 args[6]; //pass raw syscall args now and we map them later in syscall helpers
+    u64 args[6]; //raw args
 };
 
 BPF_PERF_OUTPUT(events); //perf buffer
 //perf buffer is efficient way to transfer huge amoutns of events 
 //uses shared memory between kernel and process to quickly let it write and let us read 
 
-TRACEPOINT_PROBE(raw_syscalls, sys_enter) // tracepoint is just the callback for when something happens in the kernal
-//ebpf just so happens to have sys_enter callback which lets us have an event callback for each syscall
+TRACEPOINT_PROBE(raw_syscalls, sys_enter) // tracepoint is just the callback for when something happens in the kernal (sys_enter is the event)
 {
     struct syscall_evt evt = {};
     u64 pid_tgid = bpf_get_current_pid_tgid();
 
-    evt.pid = pid_tgid >> 32; //upper 32 bits are the pid
+    evt.pid = pid_tgid >> 32; //upper 32 bits are the pid https://docs.ebpf.io/linux/helper-function/bpf_get_current_pid_tgid/
     evt.id  = args->id; //syscall id 
-    //later we map the id to the name in syscall_helpers
 
     #pragma unroll //unroll loop like inline just for ebpf verifier
     for (int i = 0; i < 6; i++) {
